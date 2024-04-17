3f3b4517d602d8ff4fe529e886987d99553ec5c6
lorenjerickson <lorenjerickson@gmail.com>
2024-04-17 12:49:35 -0700

    chore: package cleanup (#8)

--- apps/web/vite.config.mjs
+++ apps/web/vite.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
-  },
+export default defineConfig(async () => {
+  return {
+    plugins: [react(), tsconfigPaths()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.mjs',
+    },
+  }
 });
