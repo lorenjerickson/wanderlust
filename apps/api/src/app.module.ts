303d8852dce0277d506970bac74d0312f3916a27
lorenjerickson <lorenjerickson@gmail.com>
2024-04-17 12:51:31 -0700

    feat: add mongoose (#9)

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MediaModule } from './library/media/media.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/wanderlust'), MediaModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
