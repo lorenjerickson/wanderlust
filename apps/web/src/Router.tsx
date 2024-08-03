99752a1881101b0952cb8e418adf9890f934b6f7
Loren Erickson <lorenjerickson@gmail.com>
2024-08-03 12:48:33 -0700

    fix: stuff

--- apps/web/src/Router.tsx
+++ apps/web/src/Router.tsx
@@ -1,47 +1,70 @@
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import { Suspense } from 'react';
-import { Layout } from './Layout';
+import { Navigate, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import { mediaRoutes } from '@wanderlust/media';
+import { useUserSession } from '@wanderlust/ui';
+import { SideLayout } from './components/SideLayout/SideLayout';
+import { TopLayout } from './components/TopLayout/TopLayout';
+import Configure from './pages/Configure/Configure';
+import configureRoutes from './pages/Configure/routes';
+import Design from './pages/Design/Design';
+import Play from './pages/Play/Play';
 import { Welcome } from './pages/Welcome/Welcome';
 import { LoginPage } from './pages/login/Login';
 
-console.log(mediaRoutes);
+const ProtectedRoutes = () => {
+  let { isLoggedIn } = useUserSession();
+  return isLoggedIn() ? <Outlet /> : <Navigate to="/login" />;
+};
 
 const router = createBrowserRouter([
   {
-    path: 'main',
-    element: <Layout />,
+    element: <ProtectedRoutes />,
     children: [
       {
-        path: 'welcome',
-        element: <Welcome />
+        path: '/',
+        element: <TopLayout />,
+        children: [
+          {
+            path: 'welcome',
+            element: <Welcome />,
+          },
+          {
+            path: '*',
+            element: <SideLayout />,
+            children: [
+              {
+                path: 'configure',
+                element: <Configure />,
+                children: configureRoutes,
+              },
+              {
+                path: 'create',
+                element: <Design />,
+              },
+              {
+                path: 'play',
+                element: <Play />,
+              },
+            ],
+          },
+        ],
       },
-      ...mediaRoutes,
-      // {
-      //   path: 'library',
-      //   children: libraryRoutes
-      // },
-      // {
-      //   path: 'mixer',
-      //   children: mixerRoutes
-      // },
     ],
   },
   {
     path: 'login',
-    element: <LoginPage />
+    element: <LoginPage />,
   },
   {
-    path: "*", element: <Navigate to="login" />
+    path: '*',
+    element: <Navigate to="login" />,
   },
 ]);
 
 export function Router() {
   return (
-    <Suspense fallback={"Loading..."}>
+    <Suspense fallback={'Loading...'}>
       <RouterProvider router={router} />
     </Suspense>
   );
 }
-
--- apps/web/src/pages/login/Login.tsx
+++ apps/web/src/pages/login/Login.tsx
@@ -1,87 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { FormProps } from 'antd';
 import { Button, Checkbox, Form, Input } from 'antd';
-
-import classes from "./Login.module.scss";
+import { useUserSession } from '@wanderlust/ui';
 import { useNavigate } from 'react-router-dom';
 
 type FieldType = {
-    username?: string;
-    password?: string;
-    remember?: string;
+  username?: string;
+  password?: string;
+  remember?: string;
 };
 
 export const LoginPage = () => {
-    const navigate = useNavigate();
+  const { startUserSession } = useUserSession();
+  const [error, setError] = useState<string | undefined>(undefined);
+  const navigate = useNavigate();
 
-    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-        fetch('http://localhost:3000/api/login', {
-            method: 'POST',
-            body: JSON.stringify(values),
-        }).then((response) => {
-            if (response.ok) {
-                response.json().then(data => {
-                    console.log(data);
-                    navigate("welcome");
-                });
-            }
-        });
-    };
+  const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    setError(undefined);
+    startUserSession(values)
+      .then((response) => {
+        navigate('/welcome');
+      })
+      .catch((error) => {
+        console.error('Login failed', error);
+        setError('Login failed');
+      });
+  };
 
-    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 
+  return (
+    <div className={classes.page}>
+      <div className={classes.title}>Wanderlust</div>
+      <div className={classes.subtitle}>Let the adventure begin.</div>
+      <div className={classes.form}>
+        <div className={classes.login}>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 800 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input />
+            </Form.Item>
 
-    return (
-        <div className={classes.page}>
-            <div className={classes.title}>Wanderlust</div>
-            <div className={classes.subtitle}>Let the adventure begin.</div>
-            <div className={classes.form}>
-                <div className={classes.login}>
-                    <Form
-                        name="basic"
-                        labelCol={{ span: 8 }}
-                        wrapperCol={{ span: 16 }}
-                        style={{ maxWidth: 800 }}
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                    >
-                        <Form.Item<FieldType>
-                            label="Username"
-                            name="username"
-                            rules={[{ required: true, message: 'Please input your username!' }]}
-                        >
-                            <Input />
-                        </Form.Item>
+            <Form.Item<FieldType>
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-                        <Form.Item<FieldType>
-                            label="Password"
-                            name="password"
-                            rules={[{ required: true, message: 'Please input your password!' }]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
+            <Form.Item<FieldType>
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{ offset: 8, span: 16 }}
+            >
+              <Checkbox>
+                I agree to abide by the <a href="#">terms of use</a>.
+              </Checkbox>
+            </Form.Item>
 
-                        <Form.Item<FieldType>
-                            name="remember"
-                            valuePropName="checked"
-                            wrapperCol={{ offset: 8, span: 16 }}
-                        >
-                            <Checkbox>I agree to abide by the <a href="#">terms of use</a>.</Checkbox>
-                        </Form.Item>
-
-                        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                            <Button className={classes.button} htmlType="submit" size="large">
-                                Login
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
-            </div>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button className={classes.button} htmlType="submit" size="large">
+                Login
+              </Button>
+              {error && <div className={classes.error}>{error}</div>}
+            </Form.Item>
+          </Form>
         </div>
-
-    )
+      </div>
+    </div>
+  );
 };
